592b96c622e897dcc3a531176ccb5198335e1855
shivam7374 <shivamsingla491@gmail.com>
2020-05-20 22:37:23 +0530

    Comments Finally Done

--- Social-Media-Project/src/public/app/comment.js
+++ Social-Media-Project/src/public/app/comment.js
@@ -3,26 +3,41 @@ function loadPostsComments() {
     start=(document.URL.search("comments/")+9)
     end=document.URL.search("/endpoint")
     let id=parseInt(document.URL.slice(start,end))
-    $.get('http://localhost:8483/api/posts/comments/'+id, (posts) => {
+    $.get('http://localhost:8483/api/posts/comments/'+id, (post) => {
       console.log("++++++++++++++++++")
-      console.log(posts)
+      console.log(post)
       console.log("++++++++++++++++++")// working till here
-      for (let p of posts) {
+      {
         $('#posts-container').append(
           $(`
           <div class="col-4">
             <div class="card m-2">
               <div class="card-body">
-                <h5 class="card-title">${p.title}</h5>
-                <h6 class="card-subtitle mb-2 text-muted">${p.title}</h6>
+                <h5 class="card-title">${post[0].title}</h5>
+                <h6 class="card-subtitle mb-2 text-muted">${post[0].title}</h6>
                 <p class="card-text">
-                  ${p.body.substr(0, 200)}
+                  ${post[0].body}
                   <a href="#">...read more</a>
                 </p>
                 <a href="#" class="card-link">Like</a>
               </div>
             </div>
           </div>
+          <br><br><h1><div class='col-4'>COMMENTS</div></h1>               
+          
+          `)
+        )
+      }
+      for (let p of post[0].comments) {
+        $('#posts-container').append(
+          $(`
+          <br>
+          {
+            Title->>${p.title}
+            Body->>${p.body}
+          }
+          <br>
+          
           
           `)
         )
